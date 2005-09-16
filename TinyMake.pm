5b11f751f839aa84ebfb955eb5b3251a34f9d600
Walter Higgins <walter.higgins@gmail.com>
2005-09-16 15:27:56 +0000

    Import of WALTERH/TinyMake004 from CPAN.
    gitpan-cpan-distribution: TinyMake
    gitpan-cpan-version:      004
    gitpan-cpan-path:         WALTERH/TinyMake004.tar.gz
    gitpan-cpan-author:       WALTERH
    gitpan-cpan-maturity:     released

--- TinyMake.pm
+++ TinyMake.pm
@@ -1,5 +1,5 @@
 package TinyMake;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 NAME
 
@@ -9,132 +9,154 @@ TinyMake - A minimalist build language, similar in purpose to make and ant.
 
    use TinyMake ':all';
 
-   synonym all => ['codeGen', 'compile', 'dataLoad', 'test'];
+   # a file statement without a rule is like a symbolic target
+   file all => ['codeGen', 'compile', 'dataLoad', 'test'];
 
-   file { # generate code here
-     `touch $target`; 
-   } codeGen => 'database.spec';
+   file codeGen => ["database.spec"], sub { 
+     # generate code here
+     sh "touch $target";
+   } ;
 
-   file { # compile code here
-     `touch $target`;
-   } compile => 'codeGen';
+   file compile => ["codeGen"], sub { 
+     # compile code here
+     sh "touch $target";
+   } ;
 
-   file { # load data here
-	  `touch $target`;
-   } dataLoad => 'codeGen';
+   file dataLoad => ["codeGen"], sub { 
+     # load data here
+	  sh "touch $target"
+   } ;
 
-   file { # test code here
-     `touch $target`;
-   } test => ['compile', 'dataLoad'];
-
-   file { `rm compile codeGen dataLoad test` } clean => [];
+   file test => ["compile", "dataLoad"], sub { 
+     # test code here
+     sh "touch $target";
+   } ;
+   # a file statement without prerequisites will be executed
+   # if the target doesn't exist.
+   file clean => sub { 
+     # perform cleanup here
+     sh "rm compile codeGen dataLoad test" 
+   } ;
 
    make @ARGV
 
 =cut
 
 use strict;
+use File::Find ;
+
 require Exporter;
 our @ISA = ("Exporter");
-our @EXPORT_OK = qw(file synonym make tree group $target @changed %extra sh);
-our %EXPORT_TAGS = (
-						  all => \@EXPORT_OK,
-						  simple => [qw(file synonym make sh)]
-						 );
+our @EXPORT_OK = qw(file make filetree group $target @changed @sources sh);
+our %EXPORT_TAGS = (all => \@EXPORT_OK,);
 our @changed = ();
+our @sources = ();
 our $target = undef;
-our %extra = ();
+my %ts = ();
+my %tr = ();
+my $first = undef;
 
-our @tasks = ();
-
-sub sh($) {
-  my ($cmd) = @_;
-  print "$cmd\n";
-  return qx($cmd);
-}
-sub file(&@) {
-  my ($rule,$target,$sources,%extra) = @_;
-  $sources = [$sources] if (ref $sources eq '');
-  my $file = synonym ($target , $sources);
-  $file->{rule} = $rule;
-  $file->{extra} = {%extra};
-  return $file;
-}
-sub synonym(@) {
-  my ($target,$sources) = @_;
-  $sources = [$sources] if (ref $sources eq '');
-  my $synonym = {target=>$target, sources=>$sources};
-  push @tasks,$synonym;
-  return $synonym;
-}
-sub group(&@){
-  my ($coderef, $synonym, $href,%extra) = @_;
-  my @targets = keys %$href;
-  synonym($synonym, [@targets]);
-  foreach my $target (@targets){
-	 my $source = $href->{$target};
-	 my $file = synonym($target,$source);
-	 $file->{rule} = $coderef;
-	 $file->{extra} = {%extra};
+sub show (@_){
+  my ($node,$lvl) = @_;
+  $node = $first unless (defined $node);
+  $lvl = 0 unless (defined $lvl);
+  if (exists $tr{$node}){
+	 print "   " x $lvl . "*$node\n";
+  }else{
+	 print "   " x $lvl . "$node\n";
   }
-  return keys %$href;
+  $lvl+=1;
+  foreach (@{$ts{$node}}){
+	 showdeps ($_,$lvl);
+  }
+  $lvl-=1;
 }
-
-sub depgraph {
-  my ($tn,$result,$visited) = @_;
-
-  die "TinyMake does not support circular dependency '$tn'\n" 
-	 if (grep {defined $_ && $tn eq $_} @$visited);
-
-  my ($t) = grep {$_->{target} eq $tn} @tasks;
-  unless ($t == undef){
-	 push @$visited, $tn;
-
-	 foreach my $source (@{$t->{sources}}){
-		
-		depgraph($source,$result,$visited) ;
-		# 
-		# pop the source off the visited stack
-		#
-		map { $_ = $_ eq $source?undef:$_ } @$visited;
-		
+sub sh (@){
+  print "@_\n";
+  return qx(@_);
+}
+sub file {
+  my ($t,@rest) = @_;
+  $first = $t unless defined $first;
+  if (ref $rest[0] eq 'CODE'){
+	 $tr{$t} = $rest[0];
+  }else{
+	 $rest[0] = [$rest[0]] unless (ref $rest[0] eq 'ARRAY');
+	 $ts{$t} = $rest[0];
+	 if ($rest[1]){
+		$tr{$t} = $rest[1];
 	 }
-	 push (@$result, $t) unless grep {$_->{target} eq $t->{target}} @$result;
   }
-  return @$result;
 }
-sub make(@) {
-  my (@tnames) = @_;
+sub group {
+  my ($t,$href,$coderef) = @_;
+  file $_ => $href->{$_}, $coderef foreach (keys %$href);
+  file $t => [keys %$href];
+}
+sub depends {
+  my ($t,$store,$visited) = @_;
+  return if (grep { $_ eq $t} @$visited);
+  push @$visited, $t;
+  my @kids = ();
+  @kids = @{$ts{$t}} if (exists $ts{$t});
+  depends($_,$store,$visited) foreach (@kids);
+  push @$store, $t;
+  @$store;
+}
+sub sh { 
+  print "@_\n"; 
+  qx(@_);
+}
+my %cachedsourcetimes = ();
+sub sourcetimes {
+#  map {$_ => -M $_} @_;
+  my %result = ();
+  foreach my $source (@_){
+	 my $value = undef;
+	 #
+	 # if the source is not itself a target then 
+	 # cache its modification time. THis assumes
+	 # that such sources will not be modified as a side effect
+	 # of any production rule during execution!!!!
+	 #
+	 if (exists $tr{$source}){
+		$value = -M $source;
+	 }else{
+		$cachedsourcetimes{$source} = -M $source unless exists $cachedsourcetimes{$source};
+		$value = $cachedsourcetimes{$source};
+	 }
+	 $result{$source} = $value;
+  }
+  %result;
+}
+my $verbose = undef;
+sub make {
   my @result = ();
-  @tnames = ($tasks[0]->{target}) unless (@tnames);
-  foreach (@tnames){
-	 my $rebuilt = 0;
-	 my @descendants = ();
-	 my @orderedTasksWithRules = grep {defined $_->{rule}} depgraph($_,[],[]);
-	 foreach my $task (@orderedTasksWithRules) {
-		$target = $task->{target};
-		@changed = ();
-		my $exec = 0;
+  @_ = ($first) unless (@_);
+  my $exec = 0;
+  foreach (@_){
+	 my @actionables = grep {exists $tr{$_} } depends $_,[],[];
+	 foreach (@actionables){
+		$target = $_;
+		@changed = @sources = ();
+		@changed = @sources = @{$ts{$target}} if (exists $ts{$target});
+		$exec = 1;
 		if (-e $target){
-		  @changed = grep { -M $target > -M && -e} @{$task->{sources}};
-		}else{
-		  $exec = 1;
-		  @changed = @{$task->{sources}};
+		  my $targettime = -M $target;
+		  my %sourcetimes = sourcetimes @sources;
+		  $exec = @changed = grep { $sourcetimes{$_} < $targettime } @sources;
 		}
-		if ($exec or @changed) {
-		  %extra = exists $task->{extra}?%{$task->{extra}}:();
+		if ($exec){
+		  $tr{$target}->() ;
 		  push @result, $target;
-		  $task->{rule}->();
-		  $rebuilt = 1;
 		}
 	 }
-	 print "'$_' is up to date\n" unless $rebuilt;
+	 print "$_ is up-to-date\n" unless ($exec);
   }
-  return @result;
+  @result;
 }
 
-use File::Find ;
-sub tree {
+sub filetree {
   my @found = ();
   File::Find::find sub{	 push @found, $File::Find::name }, @_;
   return @found;
@@ -178,33 +200,33 @@ The equivalent perl code using TinyMake would look like this...
  .
  . 
  .
- file { # test
+ file test => ["compile","dataLoad"], sub { # test
    `touch test`;
- } test => ['compile','dataLoad'];
+ } ;
 
- file {  # generate code
+ file codeGen => "database.spec", sub {  # generate code
    `touch codeGen`;
- } codeGen => 'database.spec';
+ } ;
   
  # some more perl code
  .
  . 
  .
- file { # compile code
+ file compile => "codeGen", sub { # compile code
    `touch compile`;
- } compile => 'codeGen';
+ } ;
 
- file { # load data
+ file dataload => "codeGen", sub { # load data
    `touch dataLoad`;
- } dataload => 'codeGen';
+ } ;
 
  make @ARGV;
 
-Using TinyMake you declare a file dependency using the C<file { ... }>  subroutine.
-This subroutine accepts a bare block of code as its first parameter, a filename (the target) 
-and a list of prerequisites as its 2nd and 3rd parameters. The code block passed in as the
-1st parameter will only be executed if the target file is out of date. A target file is
-considered to be out of date if ...
+Using TinyMake you declare a file dependency using the C<file>  subroutine.
+This subroutine accepts a target filename as its first parameter,
+and an arrayref of prerequisites and a rule coderef as its 2nd and 3rd parameters. 
+The coderef passed in as the 3rd parameter will only be executed if the target file is out of date. 
+A target file is considered to be out of date if ...
 
 =over 4
 
@@ -226,29 +248,22 @@ TinyMake provides the following subroutines...
 
 =item B<file>
 
-   file { ... } $target => \@prerequisites;
+   file $target => \@prerequisites, $code_reference;
 
 The C<file> subroutine is used to declare a target, its prerequisites and a rule to invoke if the
-target file is out of date. The code inside the C<{ ... }> curly braces does not get executed 
+target file is out of date. The supplied coderef does not get executed 
 immediately. It will only be executed if the target is out of date. Typical usage would be...
 
- file {
-        `xslfm -xsl index.xsl -files bookmarks.txt site.xml > index.html`;
- } 'index.html' => ['bookmarks.txt', 'site.xml'];
+ file "index.html" => ["bookmarks.txt", "site.xml"], sub {
+   `xslfm -xsl index.xsl -files bookmarks.txt site.xml > index.html`;
+ } ;
 
 In the above example, C<index.html> is the target and both C<bookmarks.txt> and C<site.xml> are
 prerequisites. If any of these two files change then C<index.html> should be rebuilt. The rule
-to rebuild C<index.html> is the anonymous block of code supplied within the C<{ ... }> curly braces.
-If you are familiar with Perl's sort routine and its syntax ...
- 
-   sort { $a <=> $b } @files;
-
-... then you will have guessed that the initial block of code supplied to the C<file> method is not
-executed immediately. Its execution is deferred until later when C<TinyMake> has determined whether any
-prerequisite file have changed.
+to rebuild C<index.html> is the anonymous subroutine supplied as the 3rd parameter.
 
 Just like Perl's native C<sort> subroutine, TinyMake exports some global variables which have special meaning 
-within the scope of the rule block. These special variables are...
+within the scope of the supplied rule subroutine. These special variables are...
 
 =over
 
@@ -262,84 +277,31 @@ This is the list of prerequisite files which are newer than the target. This may
 prerequisites supplied - only those which have changed since the target file was last modified. This is 
 equivalent to make's automatic variable C<$?>.
 
-=item B<%extra>
 
-This is a hash of additional information supplied to the rule at declaration time. e.g.
+=item B<@sources> 
 
-   file {
-     `javac -d $extra{d} -classpath $extra{classpath} $changed[0]`
-   } '../classes/Sample.class' => ['Sample.java'],
-     d => '../classes',
-     classpath => '../libs';
-
-Any additional parameters which are passed to C<file> after the prerequisites are stored in the 
-special variable %extra for use by the rule when it is executed.
+This is the list of all prerequisite files which are dependents of the current target. This is 
+equivalent to make's automatic variable C<$^>.
 
 =back
 
 Prerequisites must be enclosed in C<[ ... ]> square brackets. If there is only one prerequisite then
-no square brackets are required. E.g. The example above can be rewritten as...
+no square brackets are required. E.g. The following two file statements are valid...
 
-   file {
-     `javac -d $extra{d} -classpath $extra{classpath} $changed[0]`
-   } '../classes/Sample.class' => 'Sample.java',
-     d => '../classes',
-     classpath => '../libs';
+   file "../classes/Sample.class" => "Sample.java", sub {
+     sh "javac @changed"
+   };
+
+...or...
+
+   file "../classes/Sample.class" => ["Sample.java"], sub {
+     sh "javac @changed"
+   };
 
 To create Ant-style tasks, simply don't bother updating or touching the target file. 
 No file modification dates are checked so the task will be executed if it is in the dependency tree for the active target.
 The C<@changed> variable will contain all of the task's prerequisites, not just those that are newer than
-the target. The following sample code illustrates how to make breakfast using TinyMake ...
-
-  use TinyMake ':all';
-  use strict;
-  file { print "BURP!\n"                   } breakfast => ["prepare","fry","serve"];
-  file { print "Adding pepper...\n"        } add_pepper => "fetch_bowl";
-  file { print "Breakfast is served !\n"   } serve => "fry";
-  file { print "Breaking eggs...\n"        } break_eggs => "fetch_bowl";
-  file { print "Fetching bowl...\n"        } fetch_bowl => undef;
-  file { print "Frying omelette...\n"      } fry => "prepare";
-  file { print "Omelette is prepared...\n" } prepare => ["break_eggs","add_pepper","whisk"];
-  file { print "Whisking ...\n"            } whisk => ["add_pepper", "break_eggs"];
-  
-  make @ARGV;
-
-In the above example, a call to C<make 'breakfast'>, will result in the following output...
-
-  Fetching bowl...
-  Breaking eggs...
-  Adding pepper...
-  Whisking ...
-  Omelette is prepared...
-  Frying omelette...
-  Breakfast is served !
-  BURP!
-
-Note that each task is executed only once even though it may be a prerequisite for many other tasks.
-Note that the sequence in which each task is defined has no bearing whatsoever on the sequence in which
-they are executed.
-
-
-=item B<synonym>
-
-    synonym $symbolicTarget => \@prerequisites
-
-A synonym is a special type of target in that it has no rule associated with it.
-Synonyms provide a handy way of creating a short alias for long and difficult-to-remember filenames.
-A synonym can be created for either a single target or multiple targets. If you are using TinyMake
-extensively you will probably create a synonym like the following...
-
- synonym all => ['generate', 'compile', 'install' ];
-
-As with the C<file> subroutine, the C<[ ]> square brackets are optional if there is only one prerequisite.
-
- synonym password => '/etc/conf/hercules/realms/database/simpleQL/passwd';
-
-...is the same as...
-
- synonym password => ['/etc/conf/hercules/realms/database/simpleQL/passwd'];
-
-
+the target. 
 
 =item B<make>
   
@@ -361,12 +323,12 @@ Which (if any) prerequisites are out of date and must be built.
 =back 
 
 If no arguments are supplied to C<make> then (like make and Ant) it assumes the first target that was defined using C<file> 
-is the target to check. For this reason you should create an 'all' file/synonym at the start of your perl script.
+is the target to check. For this reason you should create an "all" file/synonym at the start of your perl script.
 make returns a list of changed targets.
 
-=item B<tree>
+=item B<filetree>
 
-  tree @dirs
+  filetree @dirs
 
 This is a helper function which returns a list of all of the files in the specified directory and subdirectories.
 
@@ -379,7 +341,7 @@ This is a helper function which executes the supplied string using qx() after pr
 
 =item B<group>
 
- group { rule code } $symbolicTarget => \%target_source_map
+ group  $symbolicTarget => \%target_source_map, sub { rule code }
 
 Imagine a scenario in which you have a directory with a number of B<.txt> files in it. Each of the B<.txt>
 files must be converted to corresponding B<.html> files. Using standard makefile syntax you would do 
@@ -398,28 +360,28 @@ be done as follows...
 The next step would be to call C<file> for each key/value combination as follows...
 
    foreach (keys %html2txt){
-     file {
+     file $_ => $txt2html{$_}, sub {
        # convert all .txt files to .html files
        `cp $changed[0] $target`;
-     } $_ => $txt2html{$_}
+     } ;
    } 
 
 Once we've create a file target for each html file with a corresponding .txt file as the prerequisite, we 
 would probably want to create a catchall target under which to group all of the html files...
  
- synonym html [keys %html2txt];
+ file html => [keys %html2txt];
 
 We can now build all of the html files by calling C<make 'html';>. 
 Since this kind of construct is pretty common there is a shorthand way to do this...
 
-   group {
+   group html => {map {/(.*)txt$/; "$1html" => $_} glob "*.txt"}, sub {
      # convert .txt file to .html
      `cp $changed[0] $target`;
-   } html => {map {/(.*)txt$/; "$1html" => $_} glob "*.txt"};
+   } ;
 
 What this effectively does is create multiple file targets for each .html file and create a synonym called
-'html'. Remember - the rule block for a group applies to each key/value pair in the supplied hash reference, not the
-group name which is really just a synonym. The rule block above will never be called with 'html' as the
+"html". Remember - the rule block for a group applies to each key/value pair in the supplied hash reference, not the
+group name which is really just a synonym. The rule block above will never be called with "html" as the
 target. Instead it will be called for each key/value combination where key is the target and value is the
 the prerequisite/s. 
 
@@ -438,38 +400,36 @@ and construct a C<.JAR> archive file from the built tree.
    #
    # build full jar by default
    #
-   synonym default => $project_jar;
+   file default => $project_jar;
    #
    # create a map of .class to .java files
    #
-   my %dotClassDotJavaMap =  map {
+   my %CLASS_JAVA =  map {
      /$sourcepath(.*)java$/; 
      "$outputpath$1class" => $_ 
-   } grep /\.java$/, tree $sourcepath;
+   } grep /\.java$/, filetree $sourcepath;
    #
    # group the class-to-java map under the
    # 'compile' synonym
    #
-   group { 
-     `javac -d $extra{d} -classpath $extra{classpath} $changed[0]` 
-   } compile => \%dotClassDotJavaMap,
-     d => $outputpath,
-     classpath => $classpath;
+   group compile => \%CLASS_JAVA, sub { 
+     sh "javac -d $outputpath -classpath $classpath @changed" 
+   } ;
    
    #
    # rebuild the jar if any of the .class files change
    #
-   file {
-     `jar -cvf $target -C $extra{d} com`;
-   } $project_jar => [keys %dotClassDotJavaMap],
-     d => $outputpath;
+   file $project_jar => [keys %CLASS_JAVA], sub {
+     sh "jar -cvf $target -C $outputpath com";
+   } ;
+
    #
    # clean build
    #
-   file { 
-     `rm -R $outputpath/com`;
-     `rm $project_jar`; 
-   } clean => [];
+   file clean => sub { 
+     sh "rm -R $outputpath/com";
+     sh "rm $project_jar"; 
+   };
      
    make @ARGV;
 
@@ -483,16 +443,16 @@ Walter Higgins   walterh@rocketmail.com
 e.g. 
 
    # WRONG !!!
-   synonym compile => ['project.html', 'project.exe'];
-   file { ... } 'project.tgz' => 'compile';
+   file compile => ["project.html", "project.exe"];
+   file "project.tgz" => "compile", sub { ... } ;
 
 This is incorrect because TinyMake assumes that all prerequisites are files.
 The correct way to do it is like this...
 
    # CORRECT 
-   my $compile = ['project.html', 'project.exe'];
-   synonym compile => $compile;
-   file { ... } 'project.tgz' => $compile;
+   my $compile = ["project.html", "project.exe"];
+   file compile => $compile;
+   file "project.tgz" => $compile, sub { ... };
 
 Please also refer to the java compilation example above.
 
